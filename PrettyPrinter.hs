21081c509d857ba6053a3af44211d6393dcca805
Philipp Müller <descpl@yahoo.de>
2014-03-26 12:48:05 +0100

    Prettyprinter

---
+++ PrettyPrinter.hs
@@ -0,0 +1,86 @@
+module PrettyPrinter where
+
+import Expression
+
+import System.Console.ANSI
+
+setSGRFunCall = setSGR [SetColor Foreground Vivid Red]
+setSGRFunArg = setSGR [SetColor Foreground Vivid Blue]
+setSGREta = setSGR [SetColor Foreground Vivid Green]
+
+prettyPrint :: Expression -> IO ()
+prettyPrint term = do aux False term
+                      setSGR []
+                      putStrLn ""
+    where aux plbd term = case term of
+            Variable s -> putStr s
+            Abstraction eta x f -> do (if eta then setSGREta else return ())
+                                      case (x, f) of
+                                           (_, Abstraction _ y b) -> do putStr (if plbd then " " else "\\")
+                                                                        putStr x 
+                                                                        aux True f
+                                           (_, _) -> do putStr (if plbd then " " else "\\")
+                                                        putStr x
+                                                        putStr ". "
+                                                        aux True f
+                                      setSGR []
+
+            Application beta f x -> if beta
+                                    then do (case (f, x) of
+                                             (Abstraction _ _ _, Application _ _ _) -> do putStr "(" 
+                                                                                          setSGRFunCall
+                                                                                          putStr $ show f 
+                                                                                          setSGR []
+                                                                                          putStr ") (" 
+                                                                                          setSGRFunArg
+                                                                                          putStr $ show x 
+                                                                                          setSGR []
+                                                                                          putStr ")"
+                                             (Abstraction _ _ _, Abstraction _ _ _) -> do putStr "(" 
+                                                                                          setSGRFunCall
+                                                                                          putStr $ show f 
+                                                                                          setSGR []
+                                                                                          putStr ") (" 
+                                                                                          setSGRFunArg
+                                                                                          putStr $ show x 
+                                                                                          setSGR []
+                                                                                          putStr ")"
+                                             (Abstraction _ _ _, _) -> do putStr "(" 
+                                                                          setSGRFunCall
+                                                                          putStr $ show f 
+                                                                          setSGR []
+                                                                          putStr ") "
+                                                                          setSGRFunArg
+                                                                          putStr $ show x
+                                                                          setSGR []
+                                             (_, Variable _) -> do putStr $ show f
+                                                                   putStr " "
+                                                                   putStr $ show x
+                                             (_, _) -> do putStr $ show f 
+                                                          putStr " ("
+                                                          putStr $ show x
+                                                          putStr ")")
+
+                                            setSGR []
+                                    else case (f, x) of
+                                            (Abstraction _ _ _, Application _ _ _) -> do putStr "(" 
+                                                                                         aux False f 
+                                                                                         putStr ") (" 
+                                                                                         aux False x 
+                                                                                         putStr ")"
+                                            (Abstraction _ _ _, Abstraction _ _ _) -> do putStr "(" 
+                                                                                         aux False f 
+                                                                                         putStr ") (" 
+                                                                                         aux False x 
+                                                                                         putStr ")"
+                                            (Abstraction _ _ _, _) -> do putStr "(" 
+                                                                         aux False f 
+                                                                         putStr ") "
+                                                                         aux False x
+                                            (_, Variable _) -> do aux False f
+                                                                  putStr " "
+                                                                  aux False x
+                                            (_, _) -> do aux False f 
+                                                         putStr " ("
+                                                         aux False x
+                                                         putStr ")"
